ee0046d24d2827fb400280a65ac06839abdb2b22
bonnichiwa <bonnie.pham@ryerson.ca>
2015-10-13 11:21:15 -0400

    removed unnecessary code

--- app/app.js
+++ app/app.js
@@ -2,8 +2,6 @@ angular.module('calendarDemoApp', [])
 
 .controller('displayToday', function($scope) {
   var today = new Date();
-  var month = today.getMonth()+1;
-  var day = today.getDate();
   var year = today.getFullYear();
 
   var yearsThirty = [];
@@ -25,8 +23,6 @@ angular.module('calendarDemoApp', [])
     year: $scope.selectYear 
   };
 
-  console.log($scope.monthsTwelve.indexOf($scope.selectedDate.month));
-
   $scope.isCurrentMonth = function(currentMonth){
     return (currentMonth == $scope.monthsTwelve.indexOf($scope.selectedDate.month));
   };
@@ -34,29 +30,7 @@ angular.module('calendarDemoApp', [])
   $scope.$watchCollection('selectedDate', function(selectedDate){
     $scope.newDate = selectedDate.month + ', ' + selectedDate.year;
     $scope.range = CalendarRange.getMonthlyRange(new Date($scope.newDate));
-
-    $scope.today = today;
-    $scope.month = month;
-    $scope.year = year;
-    $scope.initialDay = $scope.range.first;
-    $scope.firstDay = $scope.range.start;
-    $scope.lastDay = $scope.range.end;
-    $scope.finalDay = $scope.range.last;
     $scope.days = $scope.range.days;
-
-    // $scope.initialRange = 
-    if ($scope.initialDay < $scope.firstDay) {
-      var dateDiff = Math.abs($scope.firstDay - $scope.initialDay);
-      var initialRange = Math.ceil(dateDiff / (1000 * 3600 * 24)); 
-      console.log(initialRange);
-    }
-
-    if($scope.finalDay > $scope.lastDay) {
-      var dateDiff = Math.abs($scope.finalDay - $scope.lastDay);
-      var finalRange = Math.ceil(dateDiff / (1000 * 3600 * 24)); 
-      console.log(finalRange);
-    }
-
   });
 })
 
