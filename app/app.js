9699e3d6ab561ec770d78e2adecee7d29a517f9f
bonnichiwa <bonnie.pham@ryerson.ca>
2015-10-13 11:16:57 -0400

    diff background for extra dates and changed css styling

--- app/app.js
+++ app/app.js
@@ -17,7 +17,6 @@ angular.module('calendarDemoApp', [])
   $scope.yearsThirty = yearsThirty;
   $scope.selectYear = year;
 
-
   $scope.monthsTwelve = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   $scope.selectMonth = $scope.monthsTwelve[today.getMonth()];
 
@@ -26,10 +25,14 @@ angular.module('calendarDemoApp', [])
     year: $scope.selectYear 
   };
 
-  // var range = CalendarRange.getMonthlyRange(new Date(today));
+  console.log($scope.monthsTwelve.indexOf($scope.selectedDate.month));
+
+  $scope.isCurrentMonth = function(currentMonth){
+    return (currentMonth == $scope.monthsTwelve.indexOf($scope.selectedDate.month));
+  };
 
   $scope.$watchCollection('selectedDate', function(selectedDate){
-    $scope.newDate = selectedDate.month + ', ' + selectedDate.year
+    $scope.newDate = selectedDate.month + ', ' + selectedDate.year;
     $scope.range = CalendarRange.getMonthlyRange(new Date($scope.newDate));
 
     $scope.today = today;
@@ -38,13 +41,23 @@ angular.module('calendarDemoApp', [])
     $scope.initialDay = $scope.range.first;
     $scope.firstDay = $scope.range.start;
     $scope.lastDay = $scope.range.end;
+    $scope.finalDay = $scope.range.last;
     $scope.days = $scope.range.days;
+
+    // $scope.initialRange = 
+    if ($scope.initialDay < $scope.firstDay) {
+      var dateDiff = Math.abs($scope.firstDay - $scope.initialDay);
+      var initialRange = Math.ceil(dateDiff / (1000 * 3600 * 24)); 
+      console.log(initialRange);
+    }
+
+    if($scope.finalDay > $scope.lastDay) {
+      var dateDiff = Math.abs($scope.finalDay - $scope.lastDay);
+      var finalRange = Math.ceil(dateDiff / (1000 * 3600 * 24)); 
+      console.log(finalRange);
+    }
+
   });
-
-
-
-
-
 })
 
 .directive('calendarDisplay', function() {
@@ -55,5 +68,3 @@ angular.module('calendarDemoApp', [])
     transclude: true
   }
 })
-
-// your controller and directive code go here
